f34296f35a01648edc6c50c4ee8efe0b49492e04
Satoshi Isshiki (一色 慧) <satoshi.dev@yandex.ru>
2021-11-17 11:47:56 +0700

    readme + gif

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/app.dart';
 
 void main() async {
   await Hive.initFlutter();
+  
   runApp(const TodoApp());
 }
